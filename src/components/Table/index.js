26ea548fd19b6cc4411a8e0d8344690bf9b9bc9a
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-05-13 08:26:04 +0930

    data table added

---
+++ src/components/Table/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Style.css';
+function Table(props) {
+
+    return (
+        <table className="table table-striped">
+            <thead className="thead-dark">
+                <tr className="headTable">
+                    <th>Image</th>
+                    <th>
+                        <span  onClick={() => props.sortBy('name')}>
+                             Name
+                        </span>
+                    </th>
+                    <th>Phone Number</th>
+                    <th>Email</th>
+                    <th>Date of Birth</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.employees.map((user, index) =>
+                    <tr key={index}>
+                        <td>
+                            <img src={user.image} alt="profile" />
+                        </td>
+                        <td>{user.name}</td>
+                        <td>{user.phone}</td>
+                        <td>{user.email}</td>
+                        <td>{user.dob}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    )
+}
+
+export default Table
