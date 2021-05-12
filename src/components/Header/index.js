b58f3962abc36d225f812e837fef21ff43b64f79
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-05-13 08:26:32 +0930

    page Header added

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Style.css";
+function Header(){
+    return(<nav className="navbar navbar-dark bg-dark">
+        <h1>Employee Directory</h1>
+        <h4>Click on Name for sorting</h4>
+    </nav>);
+}
+export default Header;
